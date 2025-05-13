cb4f72ee091313d76da5093fb2656c32eb453a87
Tom Brierley <tombrierley@Toms-MacBook-Pro.local>
2025-05-13 11:41:30 +0100

    Removed Duplicate field

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,23 +20,6 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "public_read" {
-  bucket = aws_s3_bucket.static_site.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource  = "${aws_s3_bucket.static_site.arn}/*"
-      }
-    ]
-  })
-}
-
 resource "aws_s3_object" "website_files" {
   for_each = fileset("${path.module}/../website", "**/*.html")
 
